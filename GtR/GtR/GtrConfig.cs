4873e472baf66867aba00cba9ad79f183ed986fd
Ezra <ezramcnichols@gmail.com>
2020-10-10 02:29:07 -0500

    Damnit windows! case matters!

--- GtR/GtR/GtrConfig.cs
+++ GtR/GtR/GtrConfig.cs
@@ -16,7 +16,7 @@ namespace GtR
 
         private static GtrConfig LoadGtrConfig()
         {
-            var configurationFile = "GtrConfig.json";
+            var configurationFile = "GtRConfig.json";
             var config = LoadConfigFromFile(configurationFile);
             if (config == null)
                 throw new InvalidOperationException("Invalid configuration file!");
