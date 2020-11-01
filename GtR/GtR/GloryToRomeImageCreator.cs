bc4846d1245aac608f7e93b2a7194cafe2650c79
Ezra <ezramcnichols@gmail.com>
2020-11-01 02:15:31 -0600

    Revert "Trying out times to see if its this font related."
    This reverts commit 8d02fa3fbf94724b0baf60fdaa755a84d449bcc8.

--- GtR/GtR/GloryToRomeImageCreator.cs
+++ GtR/GtR/GloryToRomeImageCreator.cs
@@ -54,13 +54,10 @@ namespace GtR
         {
             GtrConfig = gtrConfig;
             var fontCollection = new PrivateFontCollection();
-            //fontCollection.AddFontFile(Path.Combine(CurrentPath, "Fonts", "NeuzeitGro-BolModified.ttf"));
-            //fontCollection.AddFontFile(Path.Combine(CurrentPath, "Fonts", "NeuzeitGro-RegModified.ttf"));
-            //regularFontFamily = fontCollection.Families.Single(family => family.Name == "Neuzeit Grotesk");
-            //boldFontFamily = fontCollection.Families.Single(family => family.Name == "Neuzeit Grotesk Bold");
-            fontCollection.AddFontFile(Path.Combine(CurrentPath, "Fonts", "times.ttf"));
-            regularFontFamily = fontCollection.Families.Single();
-            boldFontFamily = fontCollection.Families.Single();
+            fontCollection.AddFontFile(Path.Combine(CurrentPath, "Fonts", "NeuzeitGro-BolModified.ttf"));
+            fontCollection.AddFontFile(Path.Combine(CurrentPath, "Fonts", "NeuzeitGro-RegModified.ttf"));
+            regularFontFamily = fontCollection.Families.Single(family => family.Name == "Neuzeit Grotesk");
+            boldFontFamily = fontCollection.Families.Single(family => family.Name == "Neuzeit Grotesk Bold");
             CardTextFont = new Font(regularFontFamily, orderCardTextFontSize, FontStyle.Regular, GraphicsUnit.Pixel);
             BoldCardTextFont = new Font(boldFontFamily, orderCardTextFontSize, FontStyle.Bold, GraphicsUnit.Pixel);
         }
